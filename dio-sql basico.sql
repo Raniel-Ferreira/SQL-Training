067ce2962c62034a1608b0acc99e838841292223
Raniel Nascimento Ferreira <73445102+Raniel-Ferreira@users.noreply.github.com>
2021-12-10 23:32:59 -0400

    Adds create table file

---
+++ dio-sql basico.sql
@@ -0,0 +1,10 @@
+CREATE TABLE pessoas {
+    id INT NOT NULL PRIMARY KEY AUTOINCREMENT;
+    nome VARCHAR (50) NOT NULL;
+    nascimento DATE;
+}
+
+INSERT INTO pessoas (nome,nascimento) VALUES ("Raniel", "1993 05 11")\
+INSERT INTO pessoas (nome,nascimento) VALUES ("Luana", "1990 10 17")\
+INSERT INTO pessoas (nome,nascimento) VALUES ("Isaac", "2012 09 07")\
+INSERT INTO pessoas (nome,nascimento) VALUES ("Luna", "2018 06 08")\
